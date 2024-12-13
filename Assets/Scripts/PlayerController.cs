7aa83db5b8b0437c2e98be15f86bf6f0beba4c33
Katzze94 <germannavasa2003@gmail.com>
2024-12-13 10:38:19 +0100

    Commit_Salto
    Salto y ground sensor integrado.
    El personaje al saltar a veces capa el movimineto, intentar solucionarlo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,23 @@ public class PlayerController : MonoBehaviour
     private string playerDirection = "right";
     private CharacterController characterController;
     private Vector3 movement;
+
+    //Jump
+    private float _jumpForce = 5;
+
+    //GroundSensor
+
+    [SerializeField] Transform _sensorPosition;
+
+    [SerializeField]  LayerMask _groundLayer;
+    
+    [SerializeField]  float _sensorRadius = 0.5f;
+
+
+    //Gravity
+
+    [SerializeField] private float  _gravity = -9.81f;
+    [SerializeField] private Vector3 _playerGravity;
     
     void Awake(){
         characterController = GetComponent<CharacterController>();
@@ -18,6 +36,12 @@ public class PlayerController : MonoBehaviour
 
     void Update(){
         PlayerMovement();
+
+        if(Input.GetButtonDown("Jump") && IsGrounded()){
+           Jump();
+        }
+
+        Gravity();
     }
 
     void PlayerMovement(){
@@ -41,5 +65,41 @@ public class PlayerController : MonoBehaviour
             characterController.SimpleMove(movement);
             return;
         }
+
+        
+
+    }
+
+
+
+   void Gravity(){
+    if(!IsGrounded())
+    {
+        _playerGravity.y += _gravity *Time.deltaTime;
+    }   
+    else if(IsGrounded() && _playerGravity.y <0 )
+    {
+        _playerGravity.y = -1;
+    }
+
+        characterController.Move(_playerGravity * Time.deltaTime);
+   }
+   
+   
+    void Jump(){
+        _playerGravity.y = Mathf.Sqrt(_jumpForce * -2 * _gravity);
+
+        
+    }
+
+    bool IsGrounded(){
+        return Physics.CheckSphere(_sensorPosition.position, _sensorRadius, _groundLayer);
+    }
+
+
+    void OnDrawGizmos(){
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(_sensorPosition.position, 0.5f);
     }
 }
